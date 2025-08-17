93edac93c8e891621d4796e007adc8efb47a5a5e
Joel Cutler <joelcutler108@gmail.com>
2025-08-17 15:19:25 +0100

    added colour macro

--- facpu/macros.py
+++ facpu/macros.py
@@ -9,6 +9,44 @@ class Macro:
         self.func = func
 
 
+def col_macro(args: list[str], line_no: int) -> str:
+    from .assembler import AssemblyError
+
+    if len(args) == 1:
+        hex_string = args[0]
+        if len(hex_string) != 6:
+            raise AssemblyError(
+                f"Macro {Style.underline}col{Style.res_underline} was expecting a 6 character hex string, but got {len(hex_string)} characters", line_no, token=hex_string
+            )
+
+        try:
+            rgb = [int(hex_string[i : i + 2], 16) for i in (0, 2, 4)]
+        except ValueError:
+            raise AssemblyError(f"Macro {Style.underline}col{Style.res_underline} hex string contains invalid characters", line_no, token=hex_string)
+
+    elif len(args) == 3:
+        rgb: list[int] = []
+        for arg in args:
+            try:
+                arg_int = int(arg)
+            except ValueError:
+                raise AssemblyError(f"Macro {Style.underline}col{Style.res_underline} RGB values contain invalid characters", line_no, token=arg)
+
+            if not (0 <= arg_int < 256):
+                raise AssemblyError(f"Macro {Style.underline}col{Style.res_underline} RGB values must be between 0 and 255", line_no, token=arg)
+
+            rgb.append(arg_int)
+
+    else:
+        raise AssemblyError(f"Macro {Style.underline}col{Style.res_underline} expects 1 or 3 params, but got {len(args)}", line_no)
+
+    scaled_rgb = (rgb[0] * 7) // 255, (rgb[1] * 7) // 255, (rgb[2] * 3) // 255
+
+    packed_scaled_rgb = (scaled_rgb[0] << 5) | (scaled_rgb[1] << 2) | (scaled_rgb[2] << 0)
+
+    return str(packed_scaled_rgb)
+
+
 class UserMacroRegistry:
     macros: dict[str, str] = {}
 
@@ -27,5 +65,6 @@ def define_macro(args: list[str], line_no: int) -> str:
 
 
 MACROS: dict[str, Macro] = {
+    "col": Macro(col_macro),
     "define": Macro(define_macro),
 }
